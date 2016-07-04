cf3d451800d3a56b80ea85b58365ff3cf4bc640f
octps <renmo9@gmail.com>
2016-07-04 23:29:21 +0900

    #2 db 接続 viewに値を渡す

---
+++ slim-skeleton/src/models/user.php
@@ -0,0 +1,25 @@
+<?
+
+require_once(__DIR__ . '/../db.php');
+require_once(__DIR__ . '/../model.php');
+require_once(__DIR__ . '/../models/user.php');
+
+class Model_User
+{
+  public static function get() {
+      $dbh = \Db::getInstance();
+      // todo try catch
+      // 例外を投げる
+      $sql = "SELECT * FROM contents";
+      $dbh->beginTransaction();
+      $sth = $dbh->prepare($sql);
+      $sth->execute();
+      $dbh->commit();
+      while ($contnet =  $sth->fetchObject()) {
+      	$contents[] = $contnet;
+      }
+
+	  return $contents;
+	}
+}
+
--- slim-skeleton/src/routes.php
+++ slim-skeleton/src/routes.php
@@ -1,7 +1,7 @@
 <?php
 // Routes
 require_once(__DIR__ . '/./db.php');
-require_once(__DIR__ . '/./controllers/test.php');
+require_once(__DIR__ . '/./controllers/user.php');
 
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
@@ -12,15 +12,12 @@ $app->get('/[{name}]', function ($request, $response, $args) {
 });
 
 
-$app->get('/hello/[{name}]', function ($request, $response, $args) {
-	$dbh = \Db::getInstance();
-	$test = test::get($dbh);
-
-    // // Sample log message
-    // $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/user/[{name}]', function ($request, $response, $args) {
+	
+	$contents = Controller_User::get();
 
     // Render index view
-    $sender = array("args" => $args, "test"=>$test);
+    $sender = array("args" => $args, "contents"=>$contents);
     return $this->renderer->render($response, 'index.phtml', $sender);
 });
 
