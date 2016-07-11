f43dc3ac2dca170df8362d5a1ab658e238083193
octps <renmo9@gmail.com>
2016-07-12 00:20:50 +0900

    #5 content get 実装

--- slim-skeleton/src/controllers/user.php
+++ slim-skeleton/src/controllers/user.php
@@ -8,7 +8,7 @@ class Controller_User
 {
   public static function get($args) {
 	  $user = Model_User::get($args);
-	  $contents = Model_Contents::get();
+	  $contents = Model_Contents::get($user->id);
 	  $userContetns = array('user'=>$user, 'contents'=>$contents);
 	  return $userContetns;
 	}
